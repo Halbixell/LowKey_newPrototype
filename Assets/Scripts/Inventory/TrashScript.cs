0e6c280a72a7a23f1857fbca13bac5a6c13cc317
Ju542le <156426407+Ju542le@users.noreply.github.com>
2024-02-10 12:35:06 +0100

    Minipicture Move and Bug Inventory to trash
    Wenn man über die Bilder im Inventory fahrt werden die Moves jetzt auch angezeigt. Man kann jetzt Moves vom Inventory in den Trash ziehen ohne bug.
    außerdem: wenn man neuen move auf alten zieht wird alter durch neuen ersetzt

--- Assets/Scripts/Inventory/TrashScript.cs
+++ Assets/Scripts/Inventory/TrashScript.cs
@@ -18,13 +18,12 @@ public class TrashScript : MonoBehaviour, IDropHandler
 
             if (draggedItem != null)
             {
-                Debug.Log(draggedItem.GetComponent<InventorySlot>());
                 if (inventoryManager.moveSlots.Contains<InventorySlot>(draggedItem.startParent.GetComponent<InventorySlot>()))
                 {
                     RectTransform trashCanRect = GetComponent<RectTransform>();
                     Vector2 localMousePosition;
                     RectTransformUtility.ScreenPointToLocalPointInRectangle(trashCanRect, Input.mousePosition, eventData.pressEventCamera, out localMousePosition);
-
+                    
                     if (trashCanRect.rect.Contains(localMousePosition))
                     {
                         itemIndex = inventoryManager.GetItemIndex(draggedItem.item);
@@ -33,22 +32,12 @@ public class TrashScript : MonoBehaviour, IDropHandler
                         {
                             InventorySlot slot = inventoryManager.inventorySlots[itemIndex];
                             DraggableItem itemInSlot = slot.GetComponentInChildren<DraggableItem>();
-                            // Überprüfen, ob das Item in den InventorySlots vorhanden ist
 
-                            if (itemInSlot != null)
-                            {
-                                //das gleiche Item ist im Inventory, also füge hinzu
-                                slot.AddItemToSlot(draggedItem);
-                                itemInSlot.count++;
-                                itemInSlot.RefreshCount();
-                                Destroy(draggedItem.gameObject);
-                            }
-                            else
-                            {
-                                // Wenn das Item nicht in den InventorySlots ist, füge es dem entsprechenden Slot hinzu
-                                inventoryManager.AddItemToSlot(draggedItem.item);
-                                Destroy(draggedItem.gameObject);
-                            }
+                            slot.AddItemToSlot(draggedItem);
+                            itemInSlot.count++;
+                            itemInSlot.RefreshCount();
+                            Destroy(draggedItem.gameObject);
+                            
 
                         }
                     }
